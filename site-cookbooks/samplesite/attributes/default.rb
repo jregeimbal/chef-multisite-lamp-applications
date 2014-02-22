5a3ca8d12a124dea39e7d2704ace3cd0c301acb3
Jonathan Regeimbal <jonathan.regeimbal@oracle.com>
2014-02-22 18:57:53 -0500

    Moved application_php_wordpress removed samplesite cookbook

--- site-cookbooks/samplesite/attributes/default.rb
+++
@@ -1,4 +0,0 @@
-normal['wordpress']['dir'] = '/srv/www/sample/samplesite.com/docs/'
-normal['wordpress']['db']['database'] = 'samplesite_wp'
-normal['wordpress']['db']['user'] = 'samplesite'
-normal['wordpress']['db']['password'] = 'x!M54$c7Us0'
